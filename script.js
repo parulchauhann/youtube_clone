103f08b7c40f2bb2efe697a2a5d8c67e816a1e64
Parul <parul1374.be23@chitkarauniversity.edu.in>
2024-02-12 14:32:23 +0530

    added responsiveness

---
+++ script.js
@@ -0,0 +1,14 @@
+// let toggle = document.querySelector('.hamburger')
+// let menu = document.querySelector('.menu')
+// let flag = false
+
+// menu.onclick = () => {
+//     let aside_links = document.querySelector('#aside_links')
+//     if(flag == false){
+//         aside_links.style.display = 'block';
+//         flag = true
+//     }
+//     else{
+//         aside_links.style.display = 'none';
+//     }
+// }
